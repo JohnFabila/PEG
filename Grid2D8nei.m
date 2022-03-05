12eec5daa8619c68c5e5546f5d5c63a0e76462ff
JohnFabila <61230092+JohnFabila@users.noreply.github.com>
2022-03-05 17:10:42 +0000

    Update Grid2D8nei.m

--- Grid2D8nei.m
+++ Grid2D8nei.m
@@ -1,32 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@JohnFabila 
-JohnFabila
-/
-PEG
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-PEG/MIX/Grid2D8nei.m
-@JohnFabila
-JohnFabila Add files via upload
-Latest commit ba1baa0 4 hours ago
- History
- 1 contributor
-16 lines (11 sloc)  644 Bytes
-   
 function [adj]=Grid2D8nei(r,c)
 diagVec1 = repmat([ones(c-1, 1); 0], r, 1);  
                                              
@@ -42,17 +13,4 @@ adj = diag(diagVec1, 1)+...
       diag(diagVec3, c)+...
       diag(diagVec4, c+1);
 adj = adj+adj.';                             
-                      
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
+                   
